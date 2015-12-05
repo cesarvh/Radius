d8b5f20e2b30da9173d0e46946503e7aa4f3294f
Cesar Villalobos <Cesarvh@Cesars-MacBook-Pro.local>
2015-12-05 13:59:06 -0800

    init

---
+++ ios-decal-proj4/ios-decal-proj4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-decal-proj4
+//
+//  Created by Cesar Villalobos on 12/5/15.
+//  Copyright © 2015 Cesar Villalobos, Nag Alluri, Kelly Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
